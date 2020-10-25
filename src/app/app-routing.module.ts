8b2a83c96746ff2ec35e78679da9592aa1d8ed36
rafaeldpereira <rafael_d_pereira@hotmail.com>
2020-10-25 19:52:11 -0300

    Página de login no angular, restando ajustar a estilização da página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 // Components
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent} from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'sobre', component: AboutComponent}
 ];
 
