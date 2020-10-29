19935c72f19abe1420e427433e0c0da150fc54d1
Renan.info <renan.info@sede.confef.org.br>
2020-10-29 14:10:59 -0300

    carrosel tela sobre

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css','./adicional.component.css']
 })
 export class QuestionComponent implements OnInit {
 
