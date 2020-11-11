1c794e3524eda70edb35a92ef248a0fa99300762
Renan Abreu <remabreu@hotmail.com>
2020-11-10 22:55:43 -0300

    envio de email atraves da tela de contato

--- src/app/pages/home/contato/contato.component.ts
+++ src/app/pages/home/contato/contato.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+import Swal from 'sweetalert2';
+
 
 
 @Component({
@@ -7,13 +12,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contato.component.css',]
 })
 export class ContatoComponent implements OnInit {
+  form: FormGroup;
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: AuthService
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      nome: ['', Validators.required],
+      email: ['', Validators.required],
+      telefone: ['', Validators.required],
+      assunto: ['', Validators.required],
+      comentario: ['', Validators.required],
+      
     
+    });
     
   }
+  submit() {
+    Swal.showLoading();
+    const contato = this.form.value;
+    
+    console.log(contato);
+   
+
+  
+     
+       this.service.contato(contato).subscribe(
+        data => {
+          Swal.fire({
+            title: 'Contato enviado com sucesso',
+            text: 'Assim que possivel entraremos em contato.',
+            confirmButtonText: `OK`,
+          }).then((result) => {
+            this.router.navigateByUrl('/contato');
+            // this.router.navigate(['users'])
+          });
+        },
+        erro => {
+          console.log(erro);
+          Swal.fire({
+            icon: 'error',
+//title: erro.error.message,
+          });
+        }
+      );
+    
+  }
+
   
 
 }
