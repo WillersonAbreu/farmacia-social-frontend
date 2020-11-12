368fc1201bf488f7746f7854b8c2c0f1b61c94f4
David Kalil Braga <dmmd10@hotmail.com>
2020-11-12 02:34:44 -0300

    integração back com home + semifiltros

--- src/app/pages/donations/donations.service.ts
+++ src/app/pages/donations/donations.service.ts
@@ -1,4 +1,6 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BaseService } from 'src/app/core/services/base.service';
 
 @Injectable({
@@ -7,5 +9,15 @@ import { BaseService } from 'src/app/core/services/base.service';
 export class DonationsService extends BaseService {
 
   baseURL = this.baseURL + "/donations";
+
+  getPagableAndSorting(pageNumber: number, pageSize: number, filter: string): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('page', pageNumber.toString());
+    params = params.append('size', pageSize.toString());
+    params = params.append('title', filter);
+    params = params.append('description', filter);
+
+    return this.api.get(`${this.baseURL}/pageable`, { params });
+  }
 
 }
